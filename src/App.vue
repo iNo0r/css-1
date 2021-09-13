ddfd43bd07b5efd3de559af7e5eafef084a587b9
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-13 19:17:27 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div>
+    <section id="section1">
+        <h1>get th freedom you desrve.</h1>
+    </section>
+    <section id="section2">
+        <h1 class="section2-title">
+            choose you plan
+        </h1>
+        <p>make sure </p>
+        <button disabled> press</button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
