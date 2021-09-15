8a79e8265b7c508869c6d01cbb4f1be2553d5149
pic <pic.ner101@gmail.com>
2021-09-15 09:14:26 +0100

    2 classes + important

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,19 @@ export default {
 </script>
 
 <style>
+/* Questions */
+/* 
+  1- how to target the elemnt which has 2 classes or id ?
+  <div class="first-class second-class">
+  .first-class.second-class {
+  }
+
+  2- how to overwrite high specifity slector ?
+    div {
+      color: red !important
+    }
+  3- how to check specifity order ?
+  chrome dev tools
+*/
 </style>
 
