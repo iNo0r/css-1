fc82aebc53aadf6cbb7c86cb33642de7237eeed5
pic <pic.ner101@gmail.com>
2021-09-16 11:04:34 +0100

    my solution

--- src/components/Packages.vue
+++ src/components/Packages.vue
@@ -32,3 +32,42 @@
 export default {};
 </script>
 
+<style scoped>
+section {
+  box-sizing: border-box;
+  background: #d2fed9;
+  display: block;
+  width: 80%;
+  text-align: left;
+  padding: 25px;
+  margin: 20px auto;
+  border: green solid 5px;
+}
+section:nth-child(3) {
+  background: white;
+}
+section:nth-child(4) {
+  background: #1a4928;
+}
+
+section:nth-child(4) > a {
+  color: white;
+}
+
+section > a {
+  text-decoration: none;
+  color: black;
+}
+section > a > h2 {
+  color: gray;
+}
+section > a > p {
+  box-sizing: border-box;
+  padding: 10px;
+  border: 1px solid #1a4928;
+  background: white;
+}
+section:nth-child(4) > a > p {
+  color: black;
+}
+</style>
